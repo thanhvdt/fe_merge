be84bf6f9dd78f5cb60ca11fd0b7ad2a32901737
thanhvdt <tthanhvuong11@gmail.com>
2023-01-01 21:27:22 +0700

    ye

--- src/components/EnPage/Result.js
+++ src/components/EnPage/Result.js
@@ -7,29 +7,18 @@ import { useState } from "react";
 import { NavItem } from "react-bootstrap";
 
 export default function Result(props) {
-  const wordTypeConverter = (type) => {
-    switch (type) {
-      case "n":
-        return "noun";
-      case "a":
-        return "adjective";
-      case "v":
-        return "verb";
-      default:
-        return type;
-    }
-  };
-
   function play1() {
+    // THE ADDR BELOW MUST BE THE DOMAIN NAME OF THE CLOUD SERVER WHEN DEPLOYED!
     var audio1 = new Audio(
-      "http://127.0.0.1:5000/static/" + props.result.en + ".mp3"
+      "http://localhost:5000/static/" + props.result.en + "1.mp3"
     );
     audio1.play();
   }
 
   function play2() {
+    // THE ADDR BELOW MUST BE THE DOMAIN NAME OF THE CLOUD SERVER WHEN DEPLOYED!
     var audio2 = new Audio(
-      "http://127.0.0.1:5000/static/" + props.result.vn + ".mp3"
+      "http://localhost:5000/static/" + props.result.vn + "2.mp3"
     );
     audio2.play();
   }
@@ -48,16 +37,9 @@ export default function Result(props) {
               </div>
               Search Result
             </header>
+            6
             <section className="section-item section-search-result">
               <div className="result-item">
-                <div class="text-center result-img-btn">Display Image</div>
-                {/* <figure class="figure result-img">
-                    <img
-                      src="../SoftCon_Frontend/assets/img/acinic_img.jpg"
-                      class="img-fluid rounded"
-                      alt="Acinic"
-                    />
-                </figure> */}
                 <div className="result-item__term">
                   <div className="result-item__term-header">
                     {props.fromEng ? (
@@ -79,7 +61,7 @@ export default function Result(props) {
                   </div>
                   <div className="result-item__term-attr">
                     {props.fromEng ? (
-                      <span>{wordTypeConverter(props.result.type)}</span>
+                      <span>{props.result.type}</span>
                     ) : (
                       <span>{props.result.type_vn}</span>
                     )}
@@ -109,7 +91,7 @@ export default function Result(props) {
                     {props.fromEng ? (
                       <span>{props.result.type_vn}</span>
                     ) : (
-                      <span>{wordTypeConverter(props.result.type)}</span>
+                      <span>{props.result.type}</span>
                     )}
                   </div>
                 </div>
